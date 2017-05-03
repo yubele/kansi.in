64d9e505c843d987f6d5848262fdc53edc942927
yusuke <yusuke@newsdict.jp>
2017-05-03 07:19:57 +0000

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #[macro_use] extern crate nickel;
 #[warn(unreachable_code)]
 use std::collections::HashMap;
-use nickel::Nickel;
+use nickel::{Nickel, HttpRouter};
+
 include!("welcome.rs");
 
 fn main() {
     let mut server = Nickel::new();
 
-    Welcome.index(&mut server);
+    #[allow(unreachable_code)]
+    server.get("/", middleware! { |_req, res|
+        let template = Welcome.index();
+        return res.render(template.path, &template.data);
+    });
 
     let _ = server.listen("127.0.0.1:6767");
 }
